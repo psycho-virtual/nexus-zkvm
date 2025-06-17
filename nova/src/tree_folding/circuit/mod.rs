e3210d4201f39a20ec672e5a87dab405d1ce6b59
John Wu <me@johnswu.net>
2025-06-17 12:47:45 -0400

    Organizng linearization augmentation code

--- nova/src/tree_folding/circuit/mod.rs
+++ nova/src/tree_folding/circuit/mod.rs
@@ -1,7 +1,9 @@
 pub mod sha256;
 pub mod sequential_sha256;
 pub mod sumcheck;
+pub mod linearization_augmented;
 
 pub use sha256::*;
 pub use sequential_sha256::*;
-pub use sumcheck::*; 
+pub use sumcheck::*;
+pub use linearization_augmented::*; 
